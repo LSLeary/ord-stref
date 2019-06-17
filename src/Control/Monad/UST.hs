ee7ce48854b12b3c378177cf5893a71a325b66ad
L. S. Leary <LSLeary@users.noreply.github.com>
2019-06-17 18:42:22 +1200

    Requalify some imports.

--- src/Control/Monad/UST.hs
+++ src/Control/Monad/UST.hs
@@ -7,25 +7,25 @@ module Control.Monad.UST
   ) where
 
 import Control.Monad.Trans.UGenT
-import qualified Control.Monad.ST as S
+import qualified Control.Monad.ST as Base
 
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Fix (MonadFix(..))
 import Control.Monad.Fail (MonadFail)
 import Control.Applicative (liftA2)
 
-newtype ST s a = U { unU :: UGenT (S.ST s) a }
+newtype ST s a = U { unU :: UGenT (Base.ST s) a }
   deriving (Functor, Applicative, Monad, MonadFix, MonadFail)
 instance Semigroup a => Semigroup (ST s a) where (<>)   = liftA2 (<>)
 instance Monoid    a => Monoid    (ST s a) where mempty = pure mempty
 
 runST :: (forall s. ST s a) -> a
-runST s = S.runST (runUGenT (unU s))
+runST s = Base.runST (runUGenT (unU s))
 
-fromBase :: S.ST s a -> ST s a
+fromBase :: Base.ST s a -> ST s a
 fromBase = U . lift
 
-toBase :: ST s a -> S.ST s a
+toBase :: ST s a -> Base.ST s a
 toBase = runUGenT . unU
 
 newUnique :: ST s Unique
@@ -34,5 +34,5 @@ newUnique = U genUnique
 fixST :: (a -> ST s a) -> ST s a
 fixST = mfix
 
-stToIO :: ST S.RealWorld a -> IO a
-stToIO = S.stToIO . toBase
+stToIO :: ST Base.RealWorld a -> IO a
+stToIO = Base.stToIO . toBase
--- src/Data/OSTRef.hs
+++ src/Data/OSTRef.hs
@@ -1,35 +1,32 @@
 
 module Data.OSTRef
-  ( STRef
-  , newSTRef
-  , readSTRef
-  , writeSTRef
-  , modifySTRef
-  , modifySTRef'
+  ( STRef, newSTRef
+  , readSTRef, writeSTRef
+  , modifySTRef, modifySTRef'
   ) where
 
-import qualified Data.STRef as S
+import qualified Data.STRef as Base
 import Control.Monad.Trans.UGenT (Unique)
 import Control.Monad.UST (ST, newUnique, fromBase)
 import Data.Ord (comparing)
 
-data STRef s a = O { refID :: Unique, ref :: S.STRef s a }
+data STRef s a = O { refID :: Unique, ref :: Base.STRef s a }
   deriving Eq
 
 instance Ord (STRef s a) where
   compare = comparing refID
 
 newSTRef :: a -> ST s (STRef s a)
-newSTRef a = O <$> newUnique <*> fromBase (S.newSTRef a)
+newSTRef a = O <$> newUnique <*> fromBase (Base.newSTRef a)
 
 readSTRef :: STRef s a -> ST s a
-readSTRef = fromBase . S.readSTRef . ref
+readSTRef = fromBase . Base.readSTRef . ref
 
 writeSTRef :: STRef s a -> a -> ST s ()
-writeSTRef r = fromBase . S.writeSTRef (ref r)
+writeSTRef r = fromBase . Base.writeSTRef (ref r)
 
 modifySTRef :: STRef s a -> (a -> a) -> ST s ()
-modifySTRef r = fromBase . S.modifySTRef (ref r)
+modifySTRef r = fromBase . Base.modifySTRef (ref r)
 
 modifySTRef' :: STRef s a -> (a -> a) -> ST s ()
-modifySTRef' r = fromBase . S.modifySTRef' (ref r)
+modifySTRef' r = fromBase . Base.modifySTRef' (ref r)
